d827036eb6931977a0d4f4b9290c6546c011401d
Johan Sveholm <jsveholm@gmail.com>
2012-08-02 11:56:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Majan::Application.config.secret_token = 'bfb93faf68c4474300728cb9d9558d9ff2dce04e53ef0d39c1056db32c6735a0e7b61a4cc582052dad279fa1ba1affa156ebe148a600d4a3f22dfcf43efc843b'
