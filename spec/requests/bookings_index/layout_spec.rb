3bdb84df50bf40458cef8d2572051e78d2e6a8ae
Johan Sveholm <jsveholm@gmail.com>
2012-08-08 10:33:57 +0900

    before changing back go detail

--- spec/requests/bookings_index/layout_spec.rb
+++ spec/requests/bookings_index/layout_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
 describe "Bookings index" do
-  before(:each) do
-    visit bookings_path(month:'2012/7')
+  context "without bookings" do
+    before(:each) do
+      visit bookings_path(month:'2012/7')
+    end
+
+    it "has a date title" do
+      page.should have_content('July')
+    end
+
+    it "has links to detail bookingss" do
+      div(:calendar).click_link('1')
+      page.current_path.should eq day_path('2012-07-01') 
+      page.should have_title('2012-07-01')
+    end
   end
 
-  it "has a date title" do
-    page.should have_content('July')
-  end
+  context "with bookings", focus:true do
+    before(:each) do
+      day = FactoryGirl.create(:day, date:Date.parse('2012-7-2'))
+      FactoryGirl.create(:booking, day:day)
+      visit bookings_path(month:'2012/7')
+    end
 
-  it "has links to detail books" do
-    div(:calendar).click_link('1')
-    page.current_path.should eq day_path('2012-07-01') 
-    page.should have_title('2012-07-01')
+    it "" do
+    end
   end
 end
